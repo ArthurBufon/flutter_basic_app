ad09b38258936039d7a36aaa75d4314d1130cb9a
Arthur <arthurbufon2052@gmail.com>
2023-05-11 21:39:35 -0300

    adicionado navbar inferior

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,6 +37,15 @@ class _RootPageState extends State<RootPage> {
           debugPrint('Teste');
         },
         child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile')
+        ],
+        onDestinationSelected: (int index){
+          currentPage = index;
+        },
       ),
     );
   }
