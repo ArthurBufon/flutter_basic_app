503aa094abe0785e4b365cc1254d068ac989401f
Arthur <arthurbufon2052@gmail.com>
2023-05-12 21:36:55 -0300

    adicionado imagem na view learn flutter

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -11,7 +11,28 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Learn Flutter'),),
+      appBar: AppBar(
+        title: const Text('Learn Flutter'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: Column(
+        children: [
+          Image.asset('images/background.jpg'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container()
+        ],
+      ),
     );
   }
 }
